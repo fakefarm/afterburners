1d196dcdc023953fef29df5b0e01400929aaa15e
Dave Woodall <dave@hireDave.me>
2013-11-21 11:06:34 -0700

    Minor styling changes

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Afterburners
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -30,12 +29,6 @@ module Afterburners
       g.stylesheets = false
       g.javascript = false
       g.coffeescript = false
-      g.helper = false
-      g.spec = false
-      g.controller_specs = false
-      g.view_specs = false
-      g.helper_specs = false
-      g.model_specs = false
     end
   end
 end
