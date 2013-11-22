fbe7c19d6510f5a1ab1613d48dfa00eaeac8bd40
Dave Woodall <dave@hireDave.me>
2013-11-21 22:01:35 -0700

    Like Ive been writing jquery for years...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,11 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+  $('.step-details').hide();
+  $('.step-help').hover(function(){
+    $('.step-details').fadeToggle();
+  });
+});
