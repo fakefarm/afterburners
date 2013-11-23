06caa7e65453d4f10badb02e5be755f09ed2df34
Dave Woodall <dave@hireDave.me>
2013-11-23 15:33:22 -0700

    Login box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,4 +8,12 @@ $(document).ready(function(){
   $('.step-help').hover(function(){
     $('.step-details').fadeToggle();
   });
+
+  $('#login').hide();
+  $('#log_in_link').mouseenter(function(){
+    $('#login').show();
+  });
+  $('#login').mouseleave(function(){
+    $('#login').hide();
+  });
 });
