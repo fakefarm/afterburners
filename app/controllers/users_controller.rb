1b523e799868e47502dfbaa4e4a3ba2d3fd07dda
Dave Woodall <dave@hireDave.me>
2013-11-23 14:27:55 -0700

    New users are auth-logged in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Registered!"
-      redirect_to root_path
+      redirect_to new_mix_path
     else
       render :new
     end
